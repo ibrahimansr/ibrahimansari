e2d7265215e603c7e57e9e6aa4c2958bee190d4c
Ibrahim Ansari <ibrahim.ansari4161@gmail.com>
2026-04-24 04:18:19 -0400

    Hmm

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
         <h1 className="mb-8" style={{ fontSize: '2rem' }}>Ibrahim Ansari</h1>
 
         <p className="mb-4" style={{ fontSize: '1.125rem', lineHeight: '1.75' }}>
-          I want to become deeply technical. I've published 2 ml papers, fine tuned models and set up rl environments at startups, and am working with my best friends to build some cool stuff this summer in toronto and sf.
+          I want to become deeply technical, so if you are doing anything research heavy, hmu! I've published 2 ml papers, fine tuned models and set up rl environments at startups, and am working with my best friends to build some cool stuff this summer in toronto and sf.
         </p>
 
         <p style={{ fontSize: '1.125rem', lineHeight: '1.75' }}>
